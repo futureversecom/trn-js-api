91c01ff8df584c145966d44442b0abeca889a3fa
Ken Vu <ken.vu@centrality.ai>
2023-06-08 22:21:33 +1200

    Add tests for `getApiOptions` and `getProvider`

---
+++ packages/api/tests/unit/getProvider.test.ts
@@ -0,0 +1,71 @@
+import { beforeEach, describe, expect, jest, test } from "@jest/globals";
+import { getLocalProvider, getProvider, getPublicProvider } from "@trnja/api/libs/getProvider";
+
+const HttpProviderMock = jest.fn();
+const WsProviderMock = jest.fn();
+
+jest.mock("@polkadot/api", () => {
+	return {
+		__esModule: true,
+		get HttpProvider() {
+			return HttpProviderMock;
+		},
+		get WsProvider() {
+			return WsProviderMock;
+		},
+	};
+});
+
+describe("getProvider", () => {
+	beforeEach(() => {
+		HttpProviderMock.mockClear();
+		WsProviderMock.mockClear();
+	});
+
+	test("creates an instance of WsProvider", () => {
+		getProvider("ws://localhost");
+		expect(WsProviderMock).toHaveBeenCalledTimes(1);
+		expect(WsProviderMock.mock.calls[0][0]).toBe("ws://localhost");
+	});
+
+	test("creates an instance of HttpProvider", async () => {
+		getProvider("http://localhost");
+		expect(HttpProviderMock).toHaveBeenCalledTimes(1);
+		expect(HttpProviderMock.mock.calls[0][0]).toBe("http://localhost");
+	});
+
+	test("throws error if `url` parameter is unrecognizable", () => {
+		expect(() => getProvider("//localhost" as unknown as `http${string}`)).toThrow();
+	});
+});
+
+describe("getPublicProvider", () => {
+	beforeEach(() => {
+		HttpProviderMock.mockClear();
+		WsProviderMock.mockClear();
+	});
+
+	test("creates an instance of WsProvider for `root` network", async () => {
+		getPublicProvider("root");
+		expect(WsProviderMock).toHaveBeenCalledTimes(1);
+		expect(WsProviderMock.mock.calls[0][0]).toBe("wss://root.rootnet.live/ws");
+	});
+
+	test("creates an instance of WsProvider for `porcini` network", async () => {
+		getPublicProvider("porcini");
+		expect(WsProviderMock).toHaveBeenCalledTimes(1);
+		expect(WsProviderMock.mock.calls[0][0]).toBe("wss://porcini.rootnet.app/ws");
+	});
+
+	test("throws error if `network` parameter is unrecognizable", async () => {
+		expect(() => getPublicProvider("foo" as unknown as `root`)).toThrow();
+	});
+});
+
+describe("getLocalProvider", () => {
+	test("creates an instance of WsProvider", () => {
+		getLocalProvider();
+		expect(WsProviderMock).toHaveBeenCalledTimes(1);
+		expect(WsProviderMock.mock.calls[0][0]).toBe("ws://127.0.0.1:9944");
+	});
+});
