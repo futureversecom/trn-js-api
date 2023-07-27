8697aaa3dfd5a0da904a2ce7f0d425716bc38e5a
Ken Vu <ken.vu@futureverse.com>
2023-07-27 20:46:44 +1200

    Merge the tests into one file to match with the impl file

---
+++ packages/evm/tests/unit/commonUtils.test.ts
@@ -0,0 +1,16 @@
+import { describe, expect, test } from "@jest/globals";
+import { assetIdToERC20Address, collectionIdToERC721Address } from "@trnja/evm/libs/commonUtils";
+
+describe("assetIdToERC20Address", () => {
+	test("returns contract address for asset id", () => {
+		const address = assetIdToERC20Address(1);
+		expect(address).toEqual("0xcCcCCccC00000001000000000000000000000000");
+	});
+});
+
+describe("collectionIdToERC721Address", () => {
+	test("returns contract address for collection id", () => {
+		const address = collectionIdToERC721Address(1);
+		expect(address).toEqual("0xaAAaAaaa00000001000000000000000000000000");
+	});
+});
--- packages/evm/tests/unit/getAddressFromCollectionId.test.ts
+++
@@ -1,9 +0,0 @@
-import { describe, expect, test } from "@jest/globals";
-import { collectionIdToERC721Address } from "@trnja/evm/libs/commonUtils";
-
-describe("collectionIdToERC721Address", () => {
-	test("returns contract address for collection id", () => {
-		const address = collectionIdToERC721Address(1);
-		expect(address).toEqual("0xaAAaAaaa00000001000000000000000000000000");
-	});
-});
