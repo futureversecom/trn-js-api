28361cc06adc418b593187ed58d0897861b372b3
Ken Vu <ken.vu@futureverse.com>
2023-07-27 20:46:58 +1200

    Update function name for consistency

--- packages/evm/src/libs/commonUtils.ts
+++ packages/evm/src/libs/commonUtils.ts
@@ -1,6 +1,6 @@
 import { getAddress } from "ethers";
 
-export const assetIdToERC20ContractAddress = (assetId: string | number): string => {
+export const assetIdToERC20Address = (assetId: string | number): string => {
 	const asset_id_hex = (+assetId).toString(16).padStart(8, "0");
 	return getAddress(`0xCCCCCCCC${asset_id_hex.toUpperCase()}000000000000000000000000`);
 };
