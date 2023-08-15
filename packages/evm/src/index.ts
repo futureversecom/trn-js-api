141a08f475ae5109bc5a8435c5e54bbef8ac47ec
Ken Vu <ken.vu@futureverse.com>
2023-08-15 21:57:53 +1200

    Restore the PR 11

--- packages/evm/src/index.ts
+++ packages/evm/src/index.ts
@@ -1,3 +1,8 @@
 export * from "./libs/commonUtils";
 export * from "./libs/constants";
+export {
+	ERC1155_PRECOMPILE_ABI as ERC1155_ABI,
+	ERC20_PRECOMPILE_ABI as ERC20_ABI,
+	ERC721_PRECOMPILE_ABI as ERC721_ABI,
+} from "./libs/constants";
 export { getPublicProviderUrl } from "./libs/getPublicProviderUrl";
