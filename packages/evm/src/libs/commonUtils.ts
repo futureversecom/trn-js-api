b1e807b03bffc87f60afe441823e9a7aeb182c46
Joey Kheireddine <joey.kheireddine@outlook.com>
2024-07-18 20:05:51 +0200

    upd: added contractAddressToNativeId function

--- packages/evm/src/libs/commonUtils.ts
+++ packages/evm/src/libs/commonUtils.ts
@@ -1,4 +1,4 @@
-import { getAddress } from "ethers";
+import { getAddress, isAddress } from "ethers";
 
 export const assetIdToERC20Address = (assetId: string | number): string => {
 	const assetIdInHex = (+assetId).toString(16).padStart(8, "0");
@@ -13,4 +13,14 @@ export const collectionIdToERC721Address = (collectionId: string | number): stri
 export const collectionIdToERC1155Address = (collectionId: string | number): string => {
 	const collectionIdInHex = (+collectionId).toString(16).padStart(8, "0");
 	return getAddress(`0xBBBBBBBB${collectionIdInHex.toUpperCase()}000000000000000000000000`);
+};
+
+export const contractAddressToNativeId = (contractAddress: `0x${string}`): number | null => {
+	if (!isAddress(contractAddress)) throw new Error("contractAddress is not a valid address");
+	const startPrefix = contractAddress?.substring(0, 10);
+	const allowed = ["0xaaaaaaaa", "0xcccccccc", "0xbbbbbbbb"];
+	if (!allowed?.includes(startPrefix?.toLowerCase())) {
+		return null;
+	}
+	return parseInt(contractAddress?.substring(10, contractAddress?.length - 24), 16);
 };
