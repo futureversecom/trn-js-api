3ed516f8946d2d1cb998974c7da246f8aa695b31
Joey Kheireddine <joey.kheireddine@outlook.com>
2024-07-19 00:51:27 +0200

    upd: reworked getPublicProviderUrl slightly

--- packages/evm/src/libs/getPublicProviderUrl.ts
+++ packages/evm/src/libs/getPublicProviderUrl.ts
@@ -2,19 +2,15 @@ export type NetworkName = "root" | "porcini" | "sprout-1" | "sprout-2";
 
 export type HttpProviderUrl = `http${string}`;
 export function getPublicProviderUrl(network: NetworkName): HttpProviderUrl {
-	if (network === "root") {
-		return "https://root.rootnet.live/archive";
-	}
-	if (network === "porcini") {
-		return "https://porcini.rootnet.app/archive";
-	}
+	const networks: Record<NetworkName, HttpProviderUrl> = {
+		"root": "https://root.rootnet.live/archive",
+		"porcini": "https://porcini.rootnet.app/archive",
+		"sprout-1": "https://porcini.devnet.rootnet.app/archive",
+		"sprout-2": "https://root.devnet.rootnet.app/archive",
+	};
 
-	if (network === "sprout-1") {
-		return "https://porcini.devnet.rootnet.app/archive";
-	}
-
-	if (network === "sprout-2") {
-		return "https://root.devnet.rootnet.app/archive";
+	if (networks[network]) {
+		return networks[network];
 	}
 
 	throw new Error(`Unrecognized network name: "${network}"`);
