62eeea7573d3d9edd700e7fe48fad97090a36138
Joey Kheireddine <joey.kheireddine@outlook.com>
2024-07-19 22:42:52 +0200

    upd: jsdoc in utils

--- packages/evm/src/libs/commonUtils.ts
+++ packages/evm/src/libs/commonUtils.ts
@@ -1,11 +1,21 @@
 import { getAddress, isAddress } from "ethers";
 import { TAddress } from "../types";
 
+/**
+ * Converts an asset's assetId to a contractAddress
+ * @param {string | number} assetId - The assetId we are converting
+ * @returns {TAddress} The contractaddress
+ */
 export const assetIdToERC20Address = (assetId: string | number): TAddress => {
 	const assetIdInHex = (+assetId).toString(16).padStart(8, "0");
 	return getAddress(`0xCCCCCCCC${assetIdInHex.toUpperCase()}000000000000000000000000`) as TAddress;
 };
 
+/**
+ * Converts a nft collectionId to a contractAddress
+ * @param {string | number} collectionId - The collectionId we are converting
+ * @returns {TAddress} The contractaddress
+ */
 export const collectionIdToERC721Address = (collectionId: string | number): TAddress => {
 	const collectionIdInHex = (+collectionId).toString(16).padStart(8, "0");
 	return getAddress(
@@ -13,6 +23,11 @@ export const collectionIdToERC721Address = (collectionId: string | number): TAdd
 	) as TAddress;
 };
 
+/**
+ * Converts a sft collectionId to a contractAddress
+ * @param {string | number} collectionId - The collectionId we are converting
+ * @returns {TAddress} The contractaddress
+ */
 export const collectionIdToERC1155Address = (collectionId: string | number): TAddress => {
 	const collectionIdInHex = (+collectionId).toString(16).padStart(8, "0");
 	return getAddress(
@@ -20,7 +35,12 @@ export const collectionIdToERC1155Address = (collectionId: string | number): TAd
 	) as TAddress;
 };
 
-export const contractAddressToNativeId = (contractAddress: `0x${string}`): number | null => {
+/**
+ * Converts a contractaddress back to it's nativeId (collectionId or assetId) if possible
+ * @param {TAddress} contractAddress - The address we are checking
+ * @returns {number | null} NativeID or null when conversion failed
+ */
+export const contractAddressToNativeId = (contractAddress: TAddress): number | null => {
 	if (!isAddress(contractAddress)) throw new Error("contractAddress is not a valid address");
 	const startPrefix = contractAddress?.substring(0, 10);
 	const allowed = ["0xaaaaaaaa", "0xbbbbbbbb", "0xcccccccc"];
@@ -30,6 +50,11 @@ export const contractAddressToNativeId = (contractAddress: `0x${string}`): numbe
 	return parseInt(contractAddress?.substring(10, contractAddress?.length - 24), 16);
 };
 
+/**
+ * Retrieves whether address is a Futurepass
+ * @param {TAddress} address - The address we are checking
+ * @returns {boolean} Whether it's a futurepass or not
+ */
 export const isFuturepass = (address: TAddress): boolean => {
 	if (address?.toLowerCase()?.startsWith("0xffffffff")) return true;
 	return false;
