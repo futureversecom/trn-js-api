ce37900141ca85d35110fe8f8aa1d3ca74d6d984
Joey Kheireddine <joey.kheireddine@outlook.com>
2024-07-19 22:09:51 +0200

    upd: futurepass registry jsdoc

--- packages/evm/src/libs/futurepassRegistry.ts
+++ packages/evm/src/libs/futurepassRegistry.ts
@@ -5,10 +5,14 @@ import {
 } from "./constants";
 import { TAddress, TProviderOrSigner } from "../types";
 
+/** Class that allows to use the futurepass registry pallet. */
 export class FuturepassRegistry {
 	provider: TProviderOrSigner;
 	contract: Contract;
-
+	/**
+	 * Initialize a new futurepass registry instance
+	 * @param {TProviderOrSigner} provider - An ethers provider.
+	 */
 	constructor(provider: TProviderOrSigner) {
 		this.provider = provider;
 		this.contract = new Contract(
@@ -20,14 +24,16 @@ export class FuturepassRegistry {
 
 	/**
 	 * Returns the contract instance
+	 * @returns {Contract} Contract instance
 	 */
-	getContract() {
+	getContract(): Contract {
 		return this.contract;
 	}
 	/**
 	 * Returns the futurepass for given address
 	 *
-	 * @param owner - The address
+	 * @param {TAddress} owner - The address
+	 * @returns {TAddress} The futurepass
 	 */
 	futurepassOf = async (owner: TAddress): Promise<TAddress> => {
 		return this.contract.futurepassOf(owner);
@@ -36,7 +42,8 @@ export class FuturepassRegistry {
 	/**
 	 * Allows for creation of a new futurepass
 	 *
-	 * @param owner - The owner of the futurepass
+	 * @param {TAddress} owner - The owner of the futurepass
+	 * @returns {TransactionResponse} Transaction object
 	 */
 	create = async (owner: TAddress): Promise<TransactionResponse> => {
 		return this.contract.create(owner);
