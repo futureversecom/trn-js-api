2f619521d8376b09d27ae81ebe3752ba95b76d4d
Joey Kheireddine <joey.kheireddine@outlook.com>
2024-07-19 22:05:32 +0200

    upd: feeProxy fully jsdoc

--- packages/evm/src/libs/feeProxy.ts
+++ packages/evm/src/libs/feeProxy.ts
@@ -2,14 +2,17 @@ import { BytesLike, Contract, TransactionResponse } from "ethers";
 import { TAddress, TProviderOrSigner } from "../types";
 import { FEE_PROXY_PRECOMPILE_ABI, FEE_PROXY_PRECOMPILE_ADDRESS } from "./constants";
 
+/** Class that allows to use the fee proxy pallet. */
 export class FeeProxy {
 	provider: TProviderOrSigner;
-	contractAddress: TAddress;
 	contract: Contract;
 
-	constructor(provider: TProviderOrSigner, contractAddress: TAddress) {
+	/**
+	 * Initialize a new fee proxy instance
+	 * @param {TProviderOrSigner} provider - An ethers provider.
+	 */
+	constructor(provider: TProviderOrSigner) {
 		this.provider = provider;
-		this.contractAddress = contractAddress;
 		this.contract = new Contract(
 			FEE_PROXY_PRECOMPILE_ADDRESS,
 			FEE_PROXY_PRECOMPILE_ABI,
@@ -19,18 +22,20 @@ export class FeeProxy {
 
 	/**
 	 * Returns the contract instance
+	 * @returns {Contract} The contract instance
 	 */
-	getContract() {
+	getContract(): Contract {
 		return this.contract;
 	}
 
 	/**
 	 * Execute a transaction with the transaction fee paid in a different currency
 	 *
-	 * @param asset - The asset used as to pay the tx fees with
-	 * @param maxPayment - The maximum amount paid for gas
-	 * @param target - The target address
-	 * @param input - The input data of the transaction (calldata)
+	 * @param {TAddress} asset - The asset used as to pay the tx fees with
+	 * @param {number} maxPayment - The maximum amount paid for gas
+	 * @param {TAddress} target - The target address
+	 * @param {BytesLike} input - The input data of the transaction (calldata)
+	 * @returns {TransactionResponse} Transaction object
 	 */
 	callWithFeePreferences = async (
 		asset: TAddress,
